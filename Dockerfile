a454c1d15f0294783bcdfaa0e97f9abdc4812fa5
Jonathan Tse <jtse@proactivesystem.com.hk>
2017-10-25 12:08:38 +0800

    Update with commit id and fix jdbc production conf

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM tomcat:8-jre8
+ARG COMMIT_ID='WRONG_COMMIT_ID'
+
+ENV COMMIT_ID ${COMMIT_ID}
 
 RUN ["rm", "-fr", "/usr/local/tomcat/webapps/ROOT"]
 COPY ./build/libs/*.war /usr/local/tomcat/webapps/ROOT.war
--- grails-app/init/grails/jenkins/pipeline/BootStrap.groovy
+++ grails-app/init/grails/jenkins/pipeline/BootStrap.groovy
@@ -1,8 +1,12 @@
 package grails.jenkins.pipeline
 
+import grails.util.Holders
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        println "commit id = " +  Holders.config.commitid
     }
     def destroy = {
     }
